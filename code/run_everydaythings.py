874c4208e9f988b2ceb1249f7d2ca7857a32a69e
Berenice Jaulmes <bjaulmes-22@gpu5.enst.fr>
2025-02-26 10:10:37 +0100

    cannot run without beaker token

---
+++ code/run_everydaythings.py
@@ -0,0 +1,7 @@
+from csp import *
+from enrich_via_constraints import *
+from check_constraints import *
+from relations import *
+from utils import *
+
+get_parts_perm('egg',['yolk','shell','white'])
